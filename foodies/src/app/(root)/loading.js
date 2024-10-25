d6f286ea0bfd45af740dca0f416040fb3a73adab
Sudhakar Bansal <111216638+SudhakarBansal@users.noreply.github.com>
2024-10-25 15:02:30 +0530

    added loading.js page

---
+++ foodies/src/app/(root)/loading.js
@@ -0,0 +1,16 @@
+export default function Loading() {
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
+        <div className="flex flex-col items-center">
+          {/* Spinner */}
+          <div className="w-16 h-16 border-4 border-orange-500 border-t-transparent rounded-full animate-spin"></div>
+          
+          {/* Loading Text */}
+          <p className="mt-4 text-lg font-semibold text-gray-700 dark:text-gray-300">
+            Loading...
+          </p>
+        </div>
+      </div>
+    );
+  }
+  
